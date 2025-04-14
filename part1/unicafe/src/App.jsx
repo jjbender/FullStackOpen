bc586d997c7304e6d81379ee1789a47d79b97f0b
jjbender <nikolaya.tretyakov@gmail.com>
2025-04-15 01:51:53 +0200

    1.7 no buttons

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
   // save clicks of each button to its own state
@@ -8,9 +8,40 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h2>Provide feedback to UniCafe</h2>
+      <button type="button" onClick={() => console.log('Good')}>Good</button>
+      <button type="button" onClick={() => console.log('Neutral')}>Neutral</button>
+      <button type="button" onClick={() => console.log('Bad')}>Bad</button>
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
+
     </div>
-  )
-}
+  );
+};
 
 export default App
+
+const Statistics = (props) => {
+  return (
+  <div>
+  <h2>Statistics</h2>
+  <Row text="good" value={props.good}/>
+  <Row text="neutral" value={props.neutral}/>
+  <Row text="bad" value={props.bad}/>
+  </div>
+  // .. add if no feedback given
+  //<button text="neutral" onClick="#">Neutral</button>
+      //<button text="bad" onClick="#">Bad</button>
+      //<Statistics good="good" neutral="neutral" bad="bad" />
+  );
+};
+
+const Row = (props) => {
+  return (
+    <p>
+      {props.text} {props.value}
+    </p>
+
+  );
+
+};
