9e99a217708c0ea9f03fa8ede33dd550cba1b615
jjbender <nikolaya.tretyakov@gmail.com>
2025-04-15 02:24:55 +0200

    1.6 - 1.8 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,15 +5,21 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+// The .toFixed(2) method in JavaScript formats a number to two decimal places and returns the result as a string.
+
+
+  const all = good + neutral + bad;
+  const average = all === 0 ? 0 : ((good - bad) / all).toFixed(2);
+  const positive = all === 0 ? 0 : ((good / all) * 100).toFixed(2);
 
   return (
     <div>
       <h2>Provide feedback to UniCafe</h2>
-      <button type="button" onClick={() => console.log('Good')}>Good</button>
-      <button type="button" onClick={() => console.log('Neutral')}>Neutral</button>
-      <button type="button" onClick={() => console.log('Bad')}>Bad</button>
+      <button type="button" onClick={() => setGood(good + 1)}>Good</button>
+      <button type="button" onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button type="button" onClick={() => setBad(bad + 1)}>Bad</button>
 
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} />
 
     </div>
   );
@@ -28,6 +34,9 @@ const Statistics = (props) => {
   <Row text="good" value={props.good}/>
   <Row text="neutral" value={props.neutral}/>
   <Row text="bad" value={props.bad}/>
+  <Row text="all" value={props.all}/>
+  <Row text="positive" value={props.average}/>
+  <Row text="average" value={props.positive + "%"}/>
   </div>
   // .. add if no feedback given
   //<button text="neutral" onClick="#">Neutral</button>
