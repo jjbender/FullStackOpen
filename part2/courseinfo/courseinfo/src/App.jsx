ddb544dd579aeb769a8cfd96d3b80a4810239483
jjbender <nikolaya.tretyakov@gmail.com>
2025-07-11 22:58:47 +0200

    2.4

--- part2/courseinfo/courseinfo/src/App.jsx
+++ part2/courseinfo/courseinfo/src/App.jsx
@@ -3,8 +3,8 @@ const Header = ({course}) => {
     <>
       <p> <strong>{course}</strong> </p> 
     </>
-  ) 
-}
+  ); 
+};
 
 const Part = ({part, exercises}) => {
   console.log(`Part - ${part}, Exercise number: ${exercises}`);
@@ -12,9 +12,8 @@ const Part = ({part, exercises}) => {
     <div>
       <p> Part "{part}".Exercises: {exercises} </p> 
     </div>
-  )
-  
-}
+  );
+};
 
 const Content = ({ parts }) => {
   console.log('Content includes parts:', parts);
@@ -25,49 +24,86 @@ const Content = ({ parts }) => {
       ))}
     </div>
   );
-}
+};
 
 const Total = ({parts}) => {
   const total = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return (<>
-<p> Total of exercises: {total}</p>
-  </>);
-}
+  return (
+    <>
+      <p> Total of exercises: {total}</p>
+   </>
+  );
+};
 
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-
-  const Course = () => {
+const Course = ({ course }) => {
+    
+    console.log('Course details:', course);
     return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
     </div>
-  )
-  }
+   );
+  };
 
-  return <Course course={course} />
-}
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  // possible without key parameter, but returns Warning: Each child in a list should have a unique "key" prop.
+
+  return (
+    <>
+    {courses.map((course) => (
+      <Course key={course.id} course={course} />
+    ))}
+    </>
+
+  );
+};
 
 export default App
